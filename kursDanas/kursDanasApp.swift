9292470c9f14d6fe1d019c7d97e79a4babf13ee0
Vojin <dvojinovic38@gmail.com>
2022-12-22 15:09:16 +0100

    Added initial project files

---
+++ kursDanas/kursDanasApp.swift
@@ -0,0 +1,17 @@
+//
+//  kursDanasApp.swift
+//  kursDanas
+//
+//  Created by Dusan Vojinovic on 22.12.22..
+//
+
+import SwiftUI
+
+@main
+struct kursDanasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
